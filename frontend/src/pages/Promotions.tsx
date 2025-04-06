9cd5f5c1b5e30b4af8e6df69ee41dbdb7b0b8af7
faatima_abidi <faatima.abidi24@gmail.com>
2025-04-06 00:14:46 -0400

    some fixes to the layout in transactions

--- frontend/src/pages/Promotions.tsx
+++ frontend/src/pages/Promotions.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Promotions:React.FC = () => {
   return (
-    <div>Promotions</div>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-semibold">Promotions</h1>
+        <label htmlFor="create-promo-drawer" className="btn btn-primary">
+          Create Promotion
+        </label>
+      </div>
   )
 }
