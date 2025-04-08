8be2244e646f4d438c86294ed1528bcc0a37a58e
lamia-alam <lamia.alam@outlook.com>
2025-04-08 18:07:55 -0400

    adding environment variables

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,9 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+}));
 
 // ADD YOUR WORK HERE
 
